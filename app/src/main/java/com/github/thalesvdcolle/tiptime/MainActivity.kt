e18c3b3de99888fc4775e3cf9e1d85987130143e
thalesvdcolle <thales.vdcolle@gmail.com>
2023-08-09 15:23:03 -0300

    Deixando o Codigo Funcional.

--- app/src/main/java/com/github/thalesvdcolle/tiptime/MainActivity.kt
+++ app/src/main/java/com/github/thalesvdcolle/tiptime/MainActivity.kt
@@ -2,10 +2,37 @@ package com.github.thalesvdcolle.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.thalesvdcolle.tiptime.databinding.ActivityMainBinding
+import java.lang.Math.ceil
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_quinze_percent -> 0.15
+            else -> 0.10
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = ceil(tip)
+        }
+        NumberFormat.getCurrencyInstance()
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.quantidade_gorgeta, formattedTip)
+
     }
 }
